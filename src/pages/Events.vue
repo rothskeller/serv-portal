008e96d7403c8c80c713103b12c252c6b9383f33
Steve Roth <steve.roth@hpe.com>
2020-01-11 11:33:56 -0800

    Remember which Events page tab user prefers.

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -5,12 +5,28 @@ Events displays the list of events.
 <template lang="pug">
 Page(title="Events" menuItem="events" noPadding)
   b-card#events-card(no-body)
-    b-tabs(card)
+    b-tabs(card v-model="tab")
       b-tab.events-tab-pane(title="Calendar" no-body)
         EventsCalendar
       b-tab.events-tab-pane(title="List" no-body)
         EventsList
 </template>
+
+<script>
+import Cookies from 'js-cookie'
+
+export default {
+  data: () => ({ tab: 0 }),
+  mounted() {
+    this.tab = Cookies.get('serv-events-list') === '1' ? 1 : 0
+  },
+  watch: {
+    tab() {
+      Cookies.set('serv-events-list', this.tab, { expires: 3650 })
+    },
+  },
+}
+</script>
 
 <style lang="stylus">
 #events-card
