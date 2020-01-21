2c4753f9007a7a829c2a5dfb562c29dd086dfe6e
Steve Roth <steve.roth@hpe.com>
2020-01-21 13:12:42 -0800

    Revise EventsList for small screens.

--- src/pages/events/EventsList.vue
+++ src/pages/events/EventsList.vue
@@ -12,16 +12,18 @@ EventsList displays the list of events.
   #events-list-spinner(v-if="loading")
     b-spinner(small)
   #events-list-table(v-else)
-    tr
-      th Date
-      th Event
-      th Location
-    tr(v-for="e in events" :key="e.id")
-      td(v-text="`${e.date}\u00A0${e.start}`")
-      td
+    .events-list-date.events-list-heading Date
+    .events-list-event.events-list-heading Event
+    .events-list-location.events-list-heading Location
+    template(v-for="e in events")
+      .events-list-date
+        span.events-list-year(v-text="e.date.substr(0, 5)")
+        span(v-text="e.date.substr(5)")
+        span.events-list-start(v-text="e.start")
+      .events-list-event
         EventTypeDots.mr-1(:types="e.types")
         router-link(:to="`/events/${e.id}`" v-text="e.name")
-      td
+      .events-list-location
         a(v-if="e.venue && e.venue.url" target="_blank" :href="e.venue.url" v-text="e.venue.name")
         span(v-else-if="e.venue" v-text="e.venue.name")
         span(v-else) TBD
@@ -71,8 +73,62 @@ export default {
 #events-list-spinner
   margin-top 1.5rem
 #events-list-table
+  display flex
+  flex-wrap wrap
   margin-top 1.5rem
-  th, td
-    padding 0.75rem 1em 0 0
-    vertical-align top
+.events-list-heading
+  display none
+  @media (min-width: 576px)
+    display block
+    font-weight bold
+.events-list-date
+  flex none
+  margin-top 0.25rem
+  width 3.5rem
+  font-variant tabular-nums
+  @media (min-width: 576px)
+    width 10rem
+.events-list-year
+  display none
+  @media (min-width: 576px)
+    display inline
+.events-list-start
+  display none
+  @media (min-width: 576px)
+    display inline
+    padding-left 0.25rem
+.events-list-event
+  flex none
+  overflow hidden
+  margin-top 0.25rem
+  width calc(100vw - 5.5rem)
+  text-overflow ellipsis
+  white-space nowrap
+  @media (min-width: 576px)
+    width calc(100vw - 18.5rem)
+    // 7rem sidebar
+    // 1.5rem tab margins
+    // 10rem date/time column
+  @media (min-width: 800px)
+    width calc(50vw - 9.25rem)
+    // above divided by 2
+  @media (min-width: 1280px)
+    width 30.75rem
+    // (80rem-18.5rem)/2
+.events-list-location
+  display none
+  @media (min-width: 800px)
+    display block
+    flex none
+    overflow hidden
+    margin-top 0.25rem
+    padding-left 0.25rem
+    width calc(50vw - 9.25rem)
+    // 7rem sidebar
+    // 1.5rem tab margins
+    // 10rem date/time column
+    text-overflow ellipsis
+    white-space nowrap
+  @media (min-width: 1280px)
+    width calc(100vw - 49.75rem)
 </style>
