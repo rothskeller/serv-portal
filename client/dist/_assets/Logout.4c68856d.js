24dcfc877a3bbd7d7d6c4ffdfd95c2b4164d5109
Steve Roth <steve@rothskeller.net>
2023-10-22 16:43:39 -0700

    Bug fix

--- client/dist/_assets/Logout.4c68856d.js
+++
@@ -1,1 +0,0 @@
-import{d as a,_ as e,Z as t,u as o,c as r,o as s}from"./index.d2aafc9a.js";var n=a({setup(){const a=o();e.post("/api/logout").then((()=>{t(),a.replace("/")}))}});const i={style:{margin:"1.5rem 0.75rem"}};n.render=function(a,e,t,o,n,u){return s(),r("div",i,"Logging out...")};export default n;
--- client/dist/_assets/page.b77350a6.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./index.d2aafc9a.js";let e;function s(s){e||(e=a("setPage")),e&&e(s)}export{s};
