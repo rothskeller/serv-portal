9773751f0dae87a53873a39c0d377d3f714a25e6
Steve Roth <steve@rothskeller.net>
2025-02-20 08:45:10 -0800

    Add link to interest form on SNAP page.

--- pages/static/snap.go
+++ pages/static/snap.go
@@ -7,15 +7,15 @@ import (
 	"sunnyvaleserv.org/portal/util/request"
 )
 
-// SNAPPage handles GET /sares requests.
+// SNAPPage handles GET /snap requests.
 func SNAPPage(r *request.Request) {
-	var user = auth.SessionUser(r, 0, false)
+	user := auth.SessionUser(r, 0, false)
 	ui.Page(r, user, ui.PageOpts{Title: "SNAP"}, func(main *htmlb.Element) {
 		main = main.A("class=static")
 		main.E("p").E("b").R(r.Loc("Sunnyvale Neighborhoods Actively Prepare (SNAP)"))
 		main.E("p").R(r.Loc("SNAP is our neighborhood disaster preparedness program.  Following a disaster, Sunnyvale residents will need to rely on each other for several days if city and county services are overwhelmed.  While our <a href=/listos up-target=main>Listos</a> program teaches preparedness for individuals and families, SNAP prepares neighbors to organize a timely response and to support each other in a disaster."))
 		main.E("p").R(r.Loc("Using the “Map Your Neighborhood” (MYN) program provided by the Washington State Emergency Management Division, we lead a two-hour meeting of around 15–25 households.  Neighbors learn the 9 Steps to take following a disaster, identify resources and skills available in their neighborhood that will be useful in a disaster response, and “map” any special challenges or people with particular needs.  As part of this model, neighbors get to know each other and are better prepared to work together responding to a disaster."))
-		main.E("p").R(r.Loc("For more information about SNAP, or to arrange a MYN meeting for your neighborhood, write to <a href=mailto:snap@sunnyvale.ca.gov target=_blank>snap@sunnyvale.ca.gov</a>."))
-		main.E("div class=staticBack").E("button class='sbtn sbtn-primary' onclick='history.back()'").R(r.Loc("Back"))
+		main.E("p").R(r.Loc("For more information about SNAP, or to arrange a MYN meeting for your neighborhood, click the button below and fill out the contact form.  Alternatively, you can write to <a href=mailto:snap@sunnyvale.ca.gov target=_blank>snap@sunnyvale.ca.gov</a>."))
+		main.E("div class=staticBack").E("a href=https://forms.gle/bXfpRsGohY9biBi87 target=_blank class='sbtn sbtn-primary'").R(r.Loc("Request Information"))
 	})
 }
